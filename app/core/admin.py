a3da86861c08117376077c8503749b303f7574fa
tarp20 <taras.piont@gmail.com>
2021-04-07 11:30:37 +0200

    some

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None,{'fields'}),
+        (),
+        (),
+    )
+
+
+admin.site.register(User, UserAdmin)
